ffc958add5d33486b97e548f8655ffc423b61aaf
echoiing <qwertyuiop7823@gmail.com>
2022-11-07 22:26:56 +0900

    initial commit

---
+++ src/me/day04/practice/Practice04.java
@@ -0,0 +1,35 @@
+package me.day04.practice;
+
+import java.util.Scanner;
+
+public class Practice04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("(x, y) : ");
+
+        String point = scanner.nextLine();
+        point = point.replace("(", "");
+        point = point.replace(")", "");
+        String[] strings = point.split(",");
+
+        double x = Double.parseDouble(strings[0]);
+        double y = Double.parseDouble(strings[1]);
+
+        if (x == 0 && y == 0) {
+            System.out.println("at center");
+        } else if (x == 0) {
+            System.out.println("on y-axis");
+        } else if (y == 0) {
+            System.out.println("on x-axis");
+        } else if (x > 0 && y > 0) {
+            System.out.println("at 1 quadrant");
+        } else if (x < 0 && y > 0) {
+            System.out.println("at 2 quadrant");
+        } else if (x < 0 && y < 0) {
+            System.out.println("at 3 quadrant");
+        } else {
+            System.out.println("at 4 quadrant");
+        }
+    }
+}
