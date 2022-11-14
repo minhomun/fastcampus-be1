ce3761ea1299fd6390e6c8e3265a4425e16bae18
echoiing <qwertyuiop7823@gmail.com>
2022-11-14 21:58:54 +0900

    feat: add code

--- src/me/day06/arrays/ArraysEqualsExample.java
+++ src/me/day06/arrays/ArraysEqualsExample.java
@@ -1,20 +1,41 @@
 package me.day06.arrays;
 
+import java.util.Arrays;
+
 public class ArraysEqualsExample {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 3, 3, 4, 5};
 
+        boolean isEquals = isEqualsArray(arr1, arr2);
+        System.out.println("isEquals = " + isEquals);
 
+        int[] copy = copyArray(arr1);
+        System.out.println("arr1 = " + arr1);
+        System.out.println("arr1 = " + Arrays.toString(arr1));
+        System.out.println("copy = " + copy);
+        System.out.println("copy = " + Arrays.toString(copy));
     }
 
     // 두 배열이 모두 동일한 원소를 가지고 있는지 확인하는 함수
     public static boolean isEqualsArray(int[] arr1, int[] arr2) {
-        return false;
+        // 예외처리
+        if (arr1.length != arr2.length) return false;
+
+        // arr1.length == arr2.length
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
     }
 
     // 하나의 배열의 원소를 복사하는 함수
     public static int[] copyArray(int[] arr) {
-        return null;
+        int[] copy = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i];
+        }
+        return copy;
     }
 }
